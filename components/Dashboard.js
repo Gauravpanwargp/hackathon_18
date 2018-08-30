a66d8cce4b4b36a9446e4780a2f40ec71c2f9ff9
Gaurav Panwar <gaurav.panwar@aexp.com>
2018-08-29 17:34:13 -0700

    dashboard icons added

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ProgressBarAndroid, StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
+import { ProgressBarAndroid, StyleSheet, Text, View, ScrollView, FlatList, Image } from 'react-native';
 import { Card, Button, Divider } from 'react-native-elements'
 import AppHeader from './AppHeader'
 
@@ -8,29 +8,59 @@ export default class Dashboard extends React.Component {
     return (
         <View style={{
           flex: 1,
-          flexDirection: 'column',
           justifyContent: 'space-between',
-          backgroundColor: 'skyblue',
+          backgroundColor: 'white',
         }}>
           <ScrollView>
-            <Card title='Wellness Index' style={{backgroundColor: 'powderblue'}}>
-              <Text>90/100</Text>
+            <Card>
+              <View style={{
+                flexDirection: 'row'
+              }}>
+                <Image
+                  style={{width: 65, height: 65}}
+                  source={require('../Media/wellness.jpeg')}
+                />
+                <Text fontSize='30' textAlign='right'>         WELLNESS INDEX: 91.2/100</Text>
+              </View>
+              <Text>                             </Text>
               <Text>You are doing a great job! Your wellness index is higher than 95% of your colleagues in AEDR.</Text>
             </Card>
-            <Card title='August stats' style={{backgroundColor: 'skyblue'}}>
+            <Card>
+              <View style={{
+                flexDirection: 'row'
+              }}>
+                <Image
+                  style={{width: 65, height: 65}}
+                  source={require('../Media/stats.png')}
+                />
+                <Text fontSize='30' textAlign='right'>         AUGUST STATS</Text>
+              </View>
               <FlatList
                 data={[
+                  {key: '                          '},
                   {key: '- Gratitudes expressed: 10'},
-                  {key: '- Blue Rewards given: 14 times and total 10,000 points'},
+                  {key: '- Blue Rewards given: 14 times & 10k points'},
                 ]}
                 renderItem={({item}) => <Text>{item.key}</Text>}
               />
             </Card>
-            <Card title='Upcoming activities' style={{backgroundColor: 'steelblue'}}>
+            <Card>
+              <View style={{
+                flexDirection: 'row'
+              }}>
+                <Image
+                  style={{width: 60, height: 65}}
+                  source={require('../Media/activities.jpg')}
+                />
+                <Text fontSize='30' textAlign='right'>         UPCOMING ACTIVITIES</Text>
+              </View>
               <FlatList
                 data={[
-                  {key: '- Meeting Ramit in CSB to talk about football'},
+                  {key: '                          '},
+                  {key: '- Meeting Michael in CSB to talk about football'},
                   {key: '- Learning networking skills from Mark Walter'},
+                  {key: '- Mentoring Jacob on React-native'},
+                  {key: '- Playing volleyball at 5pm'},
                 ]}
                 renderItem={({item}) => <Text>{item.key}</Text>}
               />
--- components/MainPage.js
+++ components/MainPage.js
@@ -1,30 +1,17 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import { Card, Button, Divider } from 'react-native-elements'
+import { Header } from 'react-native-elements'
 import Dashboard from './Dashboard'
 import AppHeader from './AppHeader'
-import Footer from './Footer'
 
 export default class MainPage extends React.Component {
   render() {
     return (
       <View style={{flex:1}}>
-        <AppHeader title="DHI"/>
-        <ScrollView>
-          <View style={styles.container}>
-            <Dashboard/>
-          </View>
-        </ScrollView>
+        <AppHeader title="Dashboard"/>
+        <Dashboard/>
       </View>
 
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingLeft: 10,
-    paddingRight: 10,
-  },
-});
